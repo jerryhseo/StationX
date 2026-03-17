6b6968d1c81c0753fc8ece15bc3803bfb6ede56d
user <user@JERRY-FURY>
2026-03-17 09:27:31 +0900

    v.3.2.0

---
+++ StationXDynamicInclude/bin/main/META-INF/resources/js/station-x-system.js
@@ -0,0 +1,31 @@
+const SXSystem ={
+	loadStartPortletModuleScript: (srcUrl)=>{
+		const oldScript = document.querySelector(`script[src^="${srcUrl}"]`);
+		if (oldScript){
+			//console.log("Old script removed...");
+			oldScript.remove();
+		}
+		
+		const newScript = document.createElement('script');
+		newScript.type = 'module';
+		newScript.src = `${srcUrl}?v=${Date.now()}`; // cache-busting
+		document.head.appendChild(newScript);
+	},
+	getUser: function(){
+		return {
+				userId: Liferay.ThemeDisplay.getUserId(),
+				userName: Liferay.ThemeDisplay.getUserName().replace(/\\u([\d\w]{4})/gi, (match, grp) => {
+					return String.fromCharCode(parseInt(grp, 16));
+				})
+		};
+	},
+	getLanguageId: function(){
+			return Liferay.ThemeDisplay.getLanguageId().replace(/_/g, "-");
+	},
+	getDefaultLanguageId: function(){
+			return Liferay.ThemeDisplay.getDefaultLanguageId().replace(/_/g, "-");
+	},
+	getAvailableLanguages: function(){
+		return Object.keys(Liferay.Language.available).map(lang=>lang.replace(/_/g, "-"));
+	}
+};
